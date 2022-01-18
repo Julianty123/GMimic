c32b6a45746b84b8b1629d8de6bff8bdb9d63a12
Julian <julian.a.p.c@hotmail.com>
2022-01-18 18:54:03 -0500

    update!

--- src/main/java/GMimic.java
+++ src/main/java/GMimic.java
@@ -9,9 +9,10 @@ import gearth.protocol.HPacket;
 import javafx.application.Platform;
 import javafx.scene.control.Alert;
 import javafx.scene.control.CheckBox;
+import javafx.scene.control.RadioButton;
 import javafx.scene.control.TextField;
+import javafx.scene.text.Text;
 import javafx.stage.Stage;
-
 import javax.swing.Timer;
 import java.nio.charset.StandardCharsets;
 import java.util.*;
@@ -19,13 +20,15 @@ import java.util.*;
 @ExtensionInfo(
         Title = "GMimic ",
         Description = "So Fucking Funny Extension!",
-        Version = "1.2",
+        Version = "1.2.0",
         Author = "Julianty"
 )
 
 public class GMimic extends ExtensionForm {
-    public CheckBox checkSpeech, checkLook, checkMotto, checkTile, checkName, checkBot;
-    public TextField textFieldDelay;
+    public CheckBox checkLook, checkMotto, checkTile, checkBoxName;
+    public TextField textFieldDelay, textFieldSaySomething, textFieldListenSay;
+    public RadioButton radioButtonBot, radioCustomSpeech, radioMimicSpeech, radioButtonOff;
+    public Text textListenSay, textSay;
 
     TreeMap<Integer,String> BotIndexAndBotName = new TreeMap<>();
     TreeMap<Integer,Integer> UserIdAndIndex = new TreeMap<>();
@@ -34,12 +37,12 @@ public class GMimic extends ExtensionForm {
     TreeMap<Integer,HGender> UserIdAndGender = new TreeMap<>();
     TreeMap<Integer,String> UserIdAndMotto = new TreeMap<>();
 
-    public String YourName;
+    public String YourName, UsertoMimic;
     public int UserId, YourId = -1;
     public int X, Y;
 
     // Timer para definir el numero de clicks
-    Timer timer1 = new Timer(1, e -> sendToServer(new HPacket("MoveAvatar", HMessage.Direction.TOSERVER, X, Y)));
+    Timer timer1 = null;
 
     @Override
     protected void initExtension() {
@@ -47,16 +50,23 @@ public class GMimic extends ExtensionForm {
         primaryStage.setOnShowing(e->{
             // sendToServer("GetOccupiedTiles") // Get RoomEntryTile
             // sendToServer("GetRoomEntryTile") // Get RoomOccupiedTiles for furnitures and the coords (could be important)
-            sendToServer(new HPacket("InfoRetrieve", HMessage.Direction.TOSERVER));
+            sendToServer(new HPacket("InfoRetrieve", HMessage.Direction.TOSERVER)); // Get your username
             sendToServer(new HPacket("GetHeightMap", HMessage.Direction.TOSERVER)); // Get Flooritems, Wallitems, etc. Without restart room
+
+            timer1 = new Timer(Integer.parseInt(textFieldDelay.getText()), t -> {
+                try {
+                    sendToServer(new HPacket("MoveAvatar", HMessage.Direction.TOSERVER, X, Y));
+                }catch (NullPointerException ignored){}
+            });
         });
 
-        // When the user close the extension (only works here!)
+        // When the user close the extension
         primaryStage.setOnCloseRequest(e -> {
             BotIndexAndBotName.clear(); UserIdAndIndex.clear(); UserIdAndName.clear();  UserIdAndFigureId.clear();
             UserIdAndGender.clear();    UserIdAndMotto.clear(); UserId = -1;    X = 0;  Y = 0;
-            checkLook.setSelected(false);   checkSpeech.setSelected(false); checkBot.setSelected(false);
-            Platform.runLater(() -> checkName.setText("Click the user to mimic")); // Platform.exit();
+            checkLook.setSelected(false);   textSay.setVisible(false);  textListenSay.setVisible(false);
+            textFieldListenSay.setDisable(true);    textFieldSaySomething.setDisable(true); radioButtonOff.setSelected(true);
+            Platform.runLater(() -> checkBoxName.setText("Click the user to mimic")); // Platform.exit();
         });
 
         intercept(HMessage.Direction.TOCLIENT, "UserObject", hMessage -> {
@@ -76,14 +86,16 @@ public class GMimic extends ExtensionForm {
                     textFieldDelay.setText(oldValue);
                 }
             }
+            timer1.setDelay(Integer.parseInt(textFieldDelay.getText()));
         });
 
         intercept(HMessage.Direction.TOSERVER, "GetSelectedBadges", hMessage -> {
             UserId = hMessage.getPacket().readInteger();
             try {
                 if(!UserIdAndName.get(UserId).equals(YourName)){
-                    if(checkName.isSelected()){ // Get user id for show the name
-                        Platform.runLater(() -> checkName.setText(UserIdAndName.get(UserId))); // This update GUI
+                    if(checkBoxName.isSelected()){ // Get user id for show the name
+                        UsertoMimic = UserIdAndName.get(UserId);
+                        Platform.runLater(() -> checkBoxName.setText("User to mimic: "+ UsertoMimic)); // This update GUI
                     }
                     if(checkLook.isSelected()){ // Copy user look
                         sendToServer(new HPacket("UpdateFigureData", HMessage.Direction.TOSERVER,
@@ -102,7 +114,7 @@ public class GMimic extends ExtensionForm {
                 HPacket hPacket = hMessage.getPacket();
                 HEntity[] roomUsersList = HEntity.parse(hPacket);
                 for (HEntity hEntity: roomUsersList){
-                    if (hEntity.getEntityType().equals(HEntityType.HABBO)){ // Si es Habbo hace esto
+                    if (hEntity.getEntityType().equals(HEntityType.HABBO)){
                         // El ID del usuario no esta en el Map (Dictionary en c#)
                         if(!UserIdAndIndex.containsKey(hEntity.getId())){
                             UserIdAndIndex.put(hEntity.getId(), hEntity.getIndex());
@@ -119,88 +131,80 @@ public class GMimic extends ExtensionForm {
                             UserIdAndMotto.replace(hEntity.getId(), hEntity.getMotto());
                         }
                     }
-                    else if(hEntity.getEntityType().equals(HEntityType.BOT)){
+                    if(hEntity.getEntityType().equals(HEntityType.BOT) || hEntity.getEntityType().equals(HEntityType.OLD_BOT)){
                         if(!BotIndexAndBotName.containsKey(hEntity.getIndex())){
                             BotIndexAndBotName.put(hEntity.getIndex(), hEntity.getName());
                         }
                     }
                 }
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
-            System.out.println(UserIdAndName);
+            } catch (Exception e) { e.printStackTrace(); }
         });
 
-        // Obtiene las coordenadas del usuario
+        // Get the user coords
         intercept(HMessage.Direction.TOCLIENT, "UserUpdate", hMessage -> {
-            /*int notImportant = hMessage.getPacket().readInteger();
+            /* int notImportant = hMessage.getPacket().readInteger();
             int UserIndex = hMessage.getPacket().readInteger();
-            X = hMessage.getPacket().readInteger(); Y = hMessage.getPacket().readInteger();
-            */
+            X = hMessage.getPacket().readInteger(); Y = hMessage.getPacket().readInteger(); */
             for (HEntityUpdate hEntityUpdate: HEntityUpdate.parse(hMessage.getPacket())){
                 if(checkTile.isSelected()){
                     try {
                         if(hEntityUpdate.getIndex() == UserIdAndIndex.get(UserId)){
-                            X = hEntityUpdate.getTile().getX(); Y = hEntityUpdate.getMovingTo().getY();
-                            System.out.println("x: " + X + ", y: " + Y); // Necesito mejorar el algoritmo
-                            sendToServer(new HPacket("MoveAvatar", HMessage.Direction.TOSERVER, X, Y));
+                            // hEntityUpdate.getMovingTo()... is more slow in this case
+                            X = hEntityUpdate.getTile().getX(); Y = hEntityUpdate.getTile().getY();
+                            // sendToServer(new HPacket("MoveAvatar", HMessage.Direction.TOSERVER, X, Y));
                         }
                     }catch (NullPointerException ignored){ }
                 }
             }
         });
 
-        // Intercepta cuando alguien te susurra
+        // Intercepts when a user whispers
         intercept(HMessage.Direction.TOCLIENT, "Whisper", hMessage -> {
             int CurrentIndex = hMessage.getPacket().readInteger();
-            //String SaySomething = hMessage.getPacket().readString(StandardCharsets.UTF_8); // read the string as UTF8
-            String SaySomething = hMessage.getPacket().readString();
-            int Ignore = hMessage.getPacket().readInteger();
+            String WhisperSomething = hMessage.getPacket().readString(StandardCharsets.UTF_8); // Supports the turkish characters
+            int Ignored = hMessage.getPacket().readInteger();
             int BubbleColor = hMessage.getPacket().readInteger();
             try {
                 Thread.sleep(Integer.parseInt(textFieldDelay.getText()));
             } catch (InterruptedException interruptedException) {
                 interruptedException.printStackTrace();
             }
-            //  That should support the turkish characters
-            HPacket hPacket = hMessage.getPacket().appendString(SaySomething, StandardCharsets.UTF_8);
-            System.out.println("usuario susurro: " + hPacket);
-            if(checkSpeech.isSelected()){
-                if(CurrentIndex == UserIdAndIndex.get(UserId)){  // Obtengo el index del usuario que susurra
-                    // SaySomething
+            if(radioMimicSpeech.isSelected()){
+                if(CurrentIndex == UserIdAndIndex.get(UserId)){  // Get the index of the whispering user
                     sendToServer(new HPacket("Whisper", HMessage.Direction.TOSERVER,
-                            checkName.getText() + " " + SaySomething, BubbleColor));
+                            UsertoMimic + " " + WhisperSomething, BubbleColor));
                 }
             }
-            if(checkBot.isSelected()){
+            if(radioButtonBot.isSelected()){
                 // Generally, bots have firs numbers as index (0, 1, 2...)
                 if(BotIndexAndBotName.containsKey(CurrentIndex)){
                     sendToServer(new HPacket("Chat", HMessage.Direction.TOSERVER,
-                            BotIndexAndBotName.get(CurrentIndex) + ": " + SaySomething, BubbleColor));
+                            BotIndexAndBotName.get(CurrentIndex) + ": " + WhisperSomething, BubbleColor));
                 }
             }
         });
 
-        // Intercepta cuando un bot o usuario habla
+        // Intercepts when a user or bot talks
         intercept(HMessage.Direction.TOCLIENT, "Chat", hMessage -> {
             int CurrentIndex = hMessage.getPacket().readInteger();
-            // String SaySomething = hMessage.getPacket().readString(StandardCharsets.UTF_8);
-            String SaySomething = hMessage.getPacket().readString();
-            System.out.println("saysomethign " + SaySomething);
-            int Ignore = hMessage.getPacket().readInteger();
+            String SaySomething = hMessage.getPacket().readString(StandardCharsets.UTF_8); // accept any character as ş ğ ç ı ü ä ö ...
+            int Ignored = hMessage.getPacket().readInteger();
             int BubbleColor = hMessage.getPacket().readInteger();
             try {
                 Thread.sleep(Integer.parseInt(textFieldDelay.getText()));
             } catch (InterruptedException interruptedException) { interruptedException.printStackTrace(); }
-            HPacket hPacket2 = hMessage.getPacket().appendString(SaySomething, StandardCharsets.UTF_8);
-            System.out.println("usuario hablo: " + hPacket2.readString());
 
-            if(checkSpeech.isSelected()){
+            if(radioMimicSpeech.isSelected()){
                 if(CurrentIndex == UserIdAndIndex.get(UserId)){  // Obtengo el index del usuario que habla y lo comparo
                     sendToServer(new HPacket("Chat", HMessage.Direction.TOSERVER, SaySomething, BubbleColor, 1));
                 }
             }
-            if(checkBot.isSelected()){
+            if(radioCustomSpeech.isSelected()){
+                if( CurrentIndex == UserIdAndIndex.get(UserId) && SaySomething.equalsIgnoreCase(textFieldListenSay.getText()) ){
+                    sendToServer(new HPacket("Chat", HMessage.Direction.TOSERVER, textFieldSaySomething.getText(), BubbleColor, 1));
+                }
+            }
+            if(radioButtonBot.isSelected()){
                 // Por lo general los bots tienen Index con los primeros numeros 0, 1, 2...
                 if(BotIndexAndBotName.containsKey(CurrentIndex)){
                     sendToServer(new HPacket("Chat", HMessage.Direction.TOSERVER,
@@ -209,34 +213,32 @@ public class GMimic extends ExtensionForm {
             }
         });
 
-        // Intercepta cuando un bot o usuario "GRITA"
+        // Intercepts when a user or bot shouts
         intercept(HMessage.Direction.TOCLIENT, "Shout", hMessage -> {
             int CurrentIndex = hMessage.getPacket().readInteger();
-            //String SaySomething = hMessage.getPacket().readString(StandardCharsets.UTF_8);
-            String SaySomething = hMessage.getPacket().readString();
+            String ShoutSomething = hMessage.getPacket().readString(StandardCharsets.UTF_8);
             int Ignore = hMessage.getPacket().readInteger();
             int BubbleColor = hMessage.getPacket().readInteger();
             try {
                 Thread.sleep(Integer.parseInt(textFieldDelay.getText()));
             } catch (InterruptedException interruptedException) { interruptedException.printStackTrace(); }
-            HPacket hPacket = hMessage.getPacket().appendString(SaySomething, StandardCharsets.UTF_8);
 
-            if(checkSpeech.isSelected()){   // Obtengo el index del usuario que habla y lo comparo
+            if(radioMimicSpeech.isSelected()){   // Obtengo el index del usuario que habla y lo comparo
                 if(CurrentIndex == UserIdAndIndex.get(UserId)){
-                    sendToServer(new HPacket("Shout", HMessage.Direction.TOSERVER, SaySomething, BubbleColor,0));
+                    sendToServer(new HPacket("Shout", HMessage.Direction.TOSERVER, ShoutSomething, BubbleColor,0));
                 }
             }
-            if(checkBot.isSelected()){
+            if(radioButtonBot.isSelected()){
                 if(BotIndexAndBotName.containsKey(CurrentIndex)){
                     sendToServer(new HPacket("Shout", HMessage.Direction.TOSERVER,
-                            BotIndexAndBotName.get(CurrentIndex) + ": " + hPacket.readString(), BubbleColor, 1));
+                            BotIndexAndBotName.get(CurrentIndex) + ": " + ShoutSomething, BubbleColor, 1));
                 }
             }
         });
 
         // When the user is typing
         intercept(HMessage.Direction.TOCLIENT, "UserTyping", hMessage -> {
-            if(checkSpeech.isSelected()){
+            if(radioMimicSpeech.isSelected()){
                 int CurrentIndex = hMessage.getPacket().readInteger();
                 int stateTyping = hMessage.getPacket().readInteger(); // "1" when starts and "0" when ends
                 if(CurrentIndex == UserIdAndIndex.get(UserId) && stateTyping == 1){
@@ -249,39 +251,57 @@ public class GMimic extends ExtensionForm {
         });
     }
 
-    public void handleCheckSpeech(){
-        if(checkSpeech.isSelected() && checkName.getText().equals("Click the user to mimic")){
-            checkSpeech.setSelected(false);
-            Alert alert = new Alert(Alert.AlertType.INFORMATION);
-            alert.setTitle("Information!");
-            alert.setHeaderText("You forgot to select a user");
-            alert.setContentText("You should select the user that you want to mimic");
+    public void handleMimicSpeech(){
+        if(radioMimicSpeech.isSelected()){
+            if(checkBoxName.getText().equals("Click the user to mimic")){
+                radioMimicSpeech.setSelected(false);
+                Alert alert = new Alert(Alert.AlertType.INFORMATION);
+                alert.setTitle("Information!");
+                alert.setHeaderText("You forgot to select a user");
+                alert.setContentText("You should select the user that you want to mimic");
 
-            Stage stage = (Stage) checkName.getScene().getWindow(); // Get the scene where is the control
-            double xAlert = stage.getX();               double yAlert = stage.getY();
-            alert.setX(xAlert + 400.0);                 alert.setY(yAlert + 10.0);
-            alert.show();
+                Stage stage = (Stage) checkBoxName.getScene().getWindow(); // Get the scene where is the control
+                double xAlert = stage.getX();               double yAlert = stage.getY();
+                alert.setX(xAlert + 400.0);                 alert.setY(yAlert + 10.0);
+                alert.show();
+            }
+            else {
+                textSay.setVisible(false);  textListenSay.setVisible(false);
+                textFieldListenSay.setDisable(true); textFieldSaySomething.setDisable(true);
+            }
         }
     }
 
-    public void handlecheckBot() {
-        if(checkBot.isSelected()){
-            checkSpeech.setSelected(false); checkSpeech.setDisable(true);
+    public void handleCustomSpeech(){
+        if(radioCustomSpeech.isSelected()){
+            textSay.setVisible(true);  textListenSay.setVisible(true);
+            textFieldListenSay.setDisable(false); textFieldSaySomething.setDisable(false);
+        }
+    }
+
+    public void handleRadioButtonBot() {
+        // i need to organize more this idk
+        /*if(radioButtonBot.isSelected()){
             checkLook.setSelected(false);   checkLook.setDisable(true);
             checkMotto.setSelected(false); checkMotto.setDisable(true);
             checkTile.setSelected(false);   checkTile.setDisable(true);
-            checkName.setSelected(false);   checkName.setDisable(true);
+            checkBoxName.setSelected(false);   checkBoxName.setDisable(true);
+            System.out.println("activado");
         }
         else {
-            checkSpeech.setDisable(false);  checkLook.setDisable(false);    checkMotto.setDisable(false);
-            checkTile.setDisable(false);    checkName.setDisable(false);
-        }
+            System.out.println("desactivado");
+            radioMimicSpeech.setDisable(false); radioCustomSpeech.setDisable(false);
+            checkLook.setDisable(false);    checkMotto.setDisable(false);
+            checkTile.setDisable(false);    checkBoxName.setDisable(false);
+        }*/
     }
 
     public void handlecheckLook() {
         if(checkLook.isSelected()){
-            sendToServer(new HPacket("UpdateFigureData", HMessage.Direction.TOSERVER,
-                    UserIdAndGender.get(UserId).toString(), UserIdAndFigureId.get(UserId)));
+            try{
+                sendToServer(new HPacket("UpdateFigureData", HMessage.Direction.TOSERVER,
+                        UserIdAndGender.get(UserId).toString(), UserIdAndFigureId.get(UserId)));
+            }catch (Exception ignored) { }
         }
     }
 
@@ -300,3 +320,5 @@ public class GMimic extends ExtensionForm {
         }
     }
 }
+
+//        if(response != null) sendToServer(new HPacket("12", HMessage.Direction.TOSERVER, 12));
